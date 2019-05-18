0c90e04400cb63a32ac616d024a363a68539de9c
Julian <jngraziano@gmail.com>
2019-05-18 02:48:50 -0300

    sidemenu-Home-tabsNOK
    SideMenu funcionando, Home con bienvenida. Tabs inicios, se necesita prueba con login.

--- comanda/src/app/app-routing.module.ts
+++ comanda/src/app/app-routing.module.ts
@@ -12,13 +12,30 @@ const routes: Routes = [
     loadChildren: './home/home.module#HomePageModule'
   },
   {
+    path: '',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
     path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: '',
     loadChildren: './list/list.module#ListPageModule'
   },
   {
     path: 'login',
     loadChildren: './login/login.module#LoginPageModule'
-  }
+  },
+  {
+    path: '',
+    loadChildren: './login/login.module#LoginPageModule'
+  },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  { path: 'first-with-tabs', loadChildren: './first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule' }
+
 
 ];
 
