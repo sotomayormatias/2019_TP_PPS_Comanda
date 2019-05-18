833ad1eac54910c1765fce8f6db9bd34bb41f7d2
msotomayor <msotomayor@TGVLENOVO78>
2019-05-18 03:28:29 -0300

    se agrega alta de producto al sidemenu

--- comanda/src/app/app.component.ts
+++ comanda/src/app/app.component.ts
@@ -21,6 +21,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Alta de producto',
+      url: '/abm-producto',
+      icon: 'add'
     }
   ];
 
