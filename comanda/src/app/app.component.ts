e869ea9a810c3c46039e9b6afb39d11894918de0
msotomayor <msotomayor@TGVLENOVO78>
2019-05-18 00:25:53 -0300

    Se crea servicio de firebase Se agrega pantalla de login

--- comanda/src/app/app.component.ts
+++ comanda/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 
 @Component({
   selector: 'app-root',
@@ -35,5 +37,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(FIREBASE_CONFIG);
   }
 }
