8c010ecdef1b59da510b0fd1e2928a0b2e8dd785
Leandro Pollola <leandro.pollola@hotmail.com>
2019-06-11 23:33:39 -0300

    ENCUESTA Y FOTO + LOGIN CON PERFILES + MENU MODIFICADO

--- comanda/src/app/encuesta-empleado/encuesta-empleado.page.ts
+++ comanda/src/app/encuesta-empleado/encuesta-empleado.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from "../services/firebase.service";
 import { Router } from '@angular/router';
+import { Camera, CameraOptions, PictureSourceType } from "@ionic-native/camera/ngx";
+import { AlertController } from '@ionic/angular';
+
+import * as firebase from "firebase";
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-encuesta-empleado',
   templateUrl: './encuesta-empleado.page.html',
@@ -8,7 +14,11 @@ import { Router } from '@angular/router';
 })
 export class EncuestaEmpleadoPage implements OnInit {
 
- 
+  captureDataUrl: Array<string>;
+  hayFotos: boolean = false;
+  cantidadFotos: number = 0;
+  datosEscaneados: any;
+  datos: any;
 
   ordenGeneral: number = 10;
   destacado: {
@@ -31,22 +41,85 @@ export class EncuestaEmpleadoPage implements OnInit {
 
   constructor(
     private dbService: FirebaseService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private alertCtrl: AlertController,
+    private camera: Camera
+  ) { 
+
+    this.captureDataUrl = new Array<string>();
+  }
 
   ngOnInit() {
 
   }
 
+  tomarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      sourceType: PictureSourceType.PHOTOLIBRARY
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.captureDataUrl.push('data:image/jpeg;base64,' + imageData);
+      this.hayFotos = true;
+      this.cantidadFotos += 1;
+    }, (err) => {
+      this.presentAlert(err);
+    });
+  }
+
+  async presentAlert(err) {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Error!',
+      message: err,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  
+
+
 
   enviar() {
-    
+
     
     let usuario = JSON.parse(sessionStorage.getItem("usuario")).correo;
   
+    // SUBIDA FOTO
+    
+    let storageRef = firebase.storage().ref();
+    let errores: number = 0;
+    let contador: number = 0;
+
+    this.captureDataUrl.forEach(foto => {
+
+      
+      let filename: string = usuario + "_" + Math.random() ;
+      const imageRef = storageRef.child(`encuestasEmp/${filename}.jpg`);
+
+      // let datos: any = { 'nromesa': this.nromesa, 'cantcomen': this.cantcomen, 'tmesa': this.tmesa, 'estado': 'libre'};
+      // this.enviar(datos);
+
+      imageRef.putString(foto, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
+      })
+        .catch(() => {
+          errores++;
+        });
+    });
+    this.captureDataUrl = new Array<string>();
+
+    // ENCUESTA Y REDIRECCION
+
+
     // ESTOY ENTRANDO 
-    if (this.ingreso == 0 ) 
-    {
+    if (this.ingreso == 0 ) {
       this.dbService.addItem("encuestasEmpleados", {
         "ordenGeneral": this.ordenGeneral,
         "destacado": this.destacado,
@@ -54,7 +127,7 @@ export class EncuestaEmpleadoPage implements OnInit {
         "limpieza": this.limpieza,
         "comentarios": this.comentarios,
         "usuario": usuario,
-        "fecha": new Date().toLocaleDateString(),
+        "fecha": new Date().toLocaleString(),
         "movimiento": "ENTRADA"
       });
   
@@ -63,9 +136,7 @@ export class EncuestaEmpleadoPage implements OnInit {
       this.ingreso = this.ingreso + 1;
       this.comentarios = "";
 
-    }
-    else 
-    {
+    } else {
       this.dbService.addItem("encuestasEmpleados", {
         "ordenGeneral": this.ordenGeneral,
         "destacado": this.destacado,
@@ -73,7 +144,7 @@ export class EncuestaEmpleadoPage implements OnInit {
         "limpieza": this.limpieza,
         "comentarios": this.comentarios,
         "usuario": usuario,
-        "fecha": new Date().toLocaleDateString(),
+        "fecha": new Date().toLocaleString(),
         "movimiento": "SALIDA"
       });
   
@@ -82,6 +153,15 @@ export class EncuestaEmpleadoPage implements OnInit {
     }
     
     
+    this.limpieza = null;
+    this.ordenGeneral = null;
+    this.destacado.puestoTrabajo = null;
+    
+    this.destacado.comunicacion = null;
+    this.destacado.companerismo = null;
+  
+    this.recomienda = null;
+    this.comentarios = null;
     
   }
 
