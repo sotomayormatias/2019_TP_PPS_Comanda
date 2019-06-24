dff909c13e89c1daff9bd41e6da7bfc7a4ac1bdc
Julian <jngraziano@gmail.com>
2019-06-24 03:55:43 -0300

    Reservas-SupConfirm

---
+++ comanda/src/app/list-reserva-conf/list-reserva-conf.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+
+
+@Component({
+  selector: 'app-list-reserva-conf',
+  templateUrl: './list-reserva-conf.page.html',
+  styleUrls: ['./list-reserva-conf.page.scss'],
+})
+export class ListReservaConfPage implements OnInit {
+
+  reservas: any[];
+
+
+  constructor(  private baseService: FirebaseService) { 
+    this.traerReservasPendientes();
+  }
+
+  ngOnInit() {
+  }
+
+  traerReservasPendientes() {
+    this.baseService.getItems('reservademesas').then(lista => {
+      this.reservas = lista
+      this.reservas = this.reservas.filter(reserva => reserva.estadoConfirmacion == "pendiente");
+    });
+  }
+
+  confirmarReserva(correo: string) {
+    let reservaConfirmado: any = this.reservas.find(cliente => cliente.correo == correo);
+    let key: string = reservaConfirmado.key;
+    // delete reservaConfirmado['key'];
+    reservaConfirmado.estadoConfirmacion = 'confirmado';
+    this.baseService.updateItem('reservademesas', key, reservaConfirmado);
+    // this.baseService.addItem('usuarios', { 'clave': clienteConfirmado.clave, 'correo': correo, 'perfil': 'cliente' });
+    // this.enviarCorreo(correo);
+    this.traerReservasPendientes();
+  }
+
+}
