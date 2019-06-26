dfa213a7b0cf30794ba020cb89b692d235f1d7d9
msotomayor <msotomayor@TGVLENOVO78>
2019-06-26 17:50:19 -0300

    modifico la propina de los pedidos del local para que sea porcentaje agrego validaciones al qr de propina

--- comanda/src/app/confirmar-entrega/confirmar-entrega.page.ts
+++ comanda/src/app/confirmar-entrega/confirmar-entrega.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from "../services/firebase.service";
-import { ToastController } from "@ionic/angular";
+import { ToastController, AlertController } from "@ionic/angular";
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
@@ -14,11 +14,15 @@ export class ConfirmarEntregaPage implements OnInit {
   pedidoDetalle: any[] = [];
   hayPedidoDelivery: boolean = false;
   hayPedidoEnLocal: boolean = false;
-  datosEscaneados: any;
+  precioTotalAnterior: number;
+  totalFinal: number;
+  propinaFinal: string;
+  keyPedidoDelivery: string; 
 
   constructor(private baseService: FirebaseService,
     private toastCtrl: ToastController,
-    private scanner: BarcodeScanner) {
+    private scanner: BarcodeScanner,
+    private alertCtrl: AlertController) {
     this.traerPedidos();
   }
 
@@ -41,6 +45,7 @@ export class ConfirmarEntregaPage implements OnInit {
 
       if (this.hayPedidoDelivery)
         this.traerDetalle(this.pedidoDelivery.id);
+        this.keyPedidoDelivery = this.pedidoDelivery.key;
     });
   }
 
@@ -52,20 +57,61 @@ export class ConfirmarEntregaPage implements OnInit {
 
   propina() {
     this.scanner.scan().then((data) => {
-      this.datosEscaneados = data;
-      let propina: string = JSON.parse(this.datosEscaneados.text);
+      this.propinaFinal = data.text;
 
-      this.pedidoDelivery.preciototal += parseInt(propina) * this.pedidoDelivery.preciototal / 100;
+      if (this.propinaFinal == '5' || this.propinaFinal == '10' || this.propinaFinal == '15' || this.propinaFinal == '20') {
+        this.precioTotalAnterior = this.pedidoDelivery.preciototal;
+        this.totalFinal = this.pedidoDelivery.preciototal + (this.pedidoDelivery.preciototal * parseInt(this.propinaFinal) / 100);
 
-      let key = this.pedidoDelivery.key;
-      delete this.pedidoDelivery['key'];
-      this.baseService.updateItem('pedidosDelivery', key, this.pedidoDelivery);
-
-      this.presentToast('Propina cargada');
-      this.traerDetalle(this.pedidoDelivery.id);
+        this.muestroAlert();
+      } else {
+        this.mostrarQRErroneo();
+      }
     }, (err) => {
-        console.log("Error: " + err);
-      });
+      console.log("Error: " + err);
+    });
+  }
+
+  async muestroAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Propina seleccionada: ' + this.propinaFinal + '%',
+      subHeader: '¿Confirma propina?',
+      message: 'Precio pedido: $' + JSON.stringify(this.precioTotalAnterior) + ' Desea agregar ' + this.propinaFinal + '%? Precio final: $' + this.totalFinal,
+
+      buttons: [
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.cargarenlaBD();
+            this.presentToast('Propina cargada');
+            this.traerDetalle(this.pedidoDelivery.id);
+          }
+        }, {
+          text: 'Cancelar',
+          role: 'cancel',
+          // icon: 'close',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  cargarenlaBD() {
+    // let key = this.pedidoDelivery.key;
+    delete this.pedidoDelivery['key'];
+    this.pedidoDelivery.preciototal = this.totalFinal;
+    this.baseService.updateItem('pedidosDelivery', this.keyPedidoDelivery, this.pedidoDelivery);
+  }
+
+  async mostrarQRErroneo() {
+    const alert = await this.alertCtrl.create({
+      header: 'El código leído no es un QR de propina',
+      message: 'Debe escanear un QR valido',
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
   confirmarEntrega() {
@@ -76,10 +122,10 @@ export class ConfirmarEntregaPage implements OnInit {
       this.baseService.updateItem('pedidos', key, this.pedidoEnLocal);
     }
     if (this.hayPedidoDelivery) {
-      let key: string = this.pedidoDelivery.key;
+      // let key: string = this.pedidoDelivery.key;
       delete this.pedidoDelivery['key'];
       this.pedidoDelivery.estado = 'cobrado';
-      this.baseService.updateItem('pedidosDelivery', key, this.pedidoDelivery);
+      this.baseService.updateItem('pedidosDelivery', this.keyPedidoDelivery, this.pedidoDelivery);
     }
     this.presentToast('Entrega confirmada');
     this.traerPedidos();
